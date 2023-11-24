61b664f5512e48b11d237635b936f4926e0bc0b7
aguiaralfredo <alfred00a@outlook.com>
2023-11-24 03:02:25 -0300

    Show more button

--- main.js
+++ main.js
@@ -28,3 +28,26 @@ function getGitHubProfileInfos(){
 }
 
 // getGitHubProfileInfos()
+
+
+// open close modal
+const learnMoreButton = document.getElementById("learnMoreButton")
+const learnMoreModal = document.querySelector(".learnMoreModal")
+const learnMoreCloseButton = document.getElementById("learnMoreCloseButton")
+
+
+let isOpenModal = false;
+
+const openModal = () => {
+    isOpenModal = true;
+    learnMoreModal.classList.add("open")
+}
+
+const closeModal = () => {
+    isOpenModal = false;
+    learnMoreModal.classList.remove("open")
+}
+
+
+learnMoreButton.addEventListener('click', openModal)
+learnMoreCloseButton.addEventListener('click', closeModal)
